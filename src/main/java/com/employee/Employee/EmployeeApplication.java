4b20ec84d30b910e931990b941d8525fb51b3328
manojpearls <manojpearls7@gmail.com>
2021-11-25 11:11:26 +0530

    Initial

---
+++ src/main/java/com/employee/Employee/EmployeeApplication.java
@@ -0,0 +1,22 @@
+package com.employee.Employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class EmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApplication.class, args);
+	}
+	
+	@RequestMapping("/test")
+	public String getMesssage() {
+		
+		return "Welcome to Spring Boot";
+	}
+
+}
